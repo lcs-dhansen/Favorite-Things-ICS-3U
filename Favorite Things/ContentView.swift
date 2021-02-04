87ae179ac1cc9117d30c4600d01535b482adfe5b
Devon Hanses <dhansen@lcs.on.ca>
2021-02-04 09:48:27 -0500

    Initial Commit

---
+++ Favorite Things/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Favorite Things
+//
+//  Created by Devon Hansen on 2021-02-04.
+//  Copyright © 2021 Devon Hansen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
