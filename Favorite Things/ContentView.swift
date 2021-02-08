c6547eeef904fed12282125038225472b5095d1d
Devon Hanses <dhansen@lcs.on.ca>
2021-02-08 11:33:57 -0500

    App update in view format

--- Favorite Things/ContentView.swift
+++ Favorite Things/ContentView.swift
@@ -12,34 +12,115 @@ struct ContentView: View {
     var body: some View {
         
         NavigationView {
-            
+        
             List {
                 
                 NavigationLink(destination: Boomer1()) {
-                    Text("Boomer1")
+                    HStack {
+                        Text("🦮")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Boomer1")
+                 
+                            Text("Photo 1")
+                                .font(.subheadline)
+                        }
+                        
+                    }
                 }
                 
                 NavigationLink(destination: Boomer2()) {
-                    Text("Boomer2")
+                    HStack {
+                        Text("🦮")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Boomer2")
+                 
+                            Text("Photo 2")
+                                .font(.subheadline)
+                        }
+                        
+                    }
                 }
-                
                 NavigationLink(destination: Boomer3()) {
-                    Text("Boomer3")
+                    HStack {
+                        Text("🦮")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Boomer3")
+                 
+                            Text("photo 3")
+                                .font(.subheadline)
+                        }
+                        
+                    }
                 }
                 NavigationLink(destination: Boomer4()) {
-                    Text("Boomer4")
+                    HStack {
+                        Text("🦮")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Boomer4")
+                 
+                            Text("photo 4")
+                                .font(.subheadline)
+                        }
+                        
+                    }
                 }
                 NavigationLink(destination: Boomer5()) {
-                    Text("Boomer5")
+                    HStack {
+                        Text("🦮")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Boomer5")
+                 
+                            Text("photo 5")
+                                .font(.subheadline)
+                        }
+                        
+                    }
                 }
                 NavigationLink(destination: Boomer6()) {
-                    Text("Boomer6")
+                    HStack {
+                        Text("🦮")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Boomer6")
+                 
+                            Text("photo 6")
+                                .font(.subheadline)
+                        }
+                        
+                    }
                 }
-                
                 NavigationLink(destination: Boomer7()) {
-                    Text("Boomer7")
+                    HStack {
+                        Text("🦮")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Boomer7")
+                 
+                            Text("photo 7")
+                                .font(.subheadline)
+                        }
+                        
+                    }
                 }
-            }
           .navigationBarTitle("Boomer")
             
         }
@@ -50,7 +131,12 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
 
 
+}
